e3ea86c30936a41f02c27775fe3ef5617311e525
surfaceowl <chris@surfaceowl.com>
2022-06-01 16:02:01 -0700

    [feat] add black formatting target file

---
+++ file_01_test_black_formatting.py
@@ -0,0 +1,8 @@
+"""
+incorrectly formatted file for black pre-commit hook to fix with a few errors that a programmer would never ever make
+"""
+
+
+def square_inputs(x):
+      x=x+1
+      return x
