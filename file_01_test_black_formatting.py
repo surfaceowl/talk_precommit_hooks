621dd2a1275533b2d419b5b616a9935118c6c809
surfaceowl <chris@surfaceowl.com>
2022-07-18 21:22:10 -0700

    incorporate feedback from moshe and james

--- file_01_test_black_formatting.py
+++ file_01_test_black_formatting.py
@@ -6,3 +6,7 @@ incorrectly formatted file for black pre-commit hook to fix with a few errors th
 def square_inputs(x):
     x = x + 1
     return x
+
+
+if __name__ == "__main__":
+    square_inputs(5)
